abaceab4e05883109588ddd8b985677e703519da
ghecko <ghecko78@gmail.com>
2020-06-30 15:39:21 +0200

    Adding git_init script

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.avrisp.flash_write.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.avrisp.flash_write.git
+git remote -v
